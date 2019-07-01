4b29832cddade5da91f89770baeada2693c09e15
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-01 22:27:13 +0500

    1

---
+++ C-1_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_1_1
+{
+    //машканцев
+//   1. Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+//  а) используя склеивание;
+//  б) используя форматированный вывод;
+//	в) используя вывод со знаком $.
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("напишите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("напишите фамилию");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("напишите возвраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("напишите рост");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("напишите вес");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            string output;
+            //задание 1.а
+            //output = " имя: " + name + " фамилия: " + lastname + " возвраст: " + age + " рост: " + height + " вес: " + weight;
+            //задание 1.б
+            //output = String.Format("имя: {0} фамилия: {1} возвраст: {2} рост:{3} вес:{4}",
+            //задание 1.в
+            //output = $"имя {name} фамилия {lastname } возвраст {age } рост {height } вес { weight}";
+            //            name,
+            //            lastname,
+            //            age,
+            //            height,
+            //            weight);
+
+            //Console.WriteLine(output);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ С-1_1_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С_1_1_3
+{
+    class Program
+    {
+        //машканцев
+        //а) Написать программу, которая подсчитывает расстояние между точками с 
+        //координатами x1, y1 и x2,y2 по формуле r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2).
+        //Вывести результат, используя спецификатор формата .2f(с двумя знаками после запятой);
+
+        static void Main(string[] args)
+        {
+            float x1 = 1f;
+            float y1 = 1f;
+            float x2 = 10f;
+            float y2 = 10f;
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("{0:F}", r);
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
